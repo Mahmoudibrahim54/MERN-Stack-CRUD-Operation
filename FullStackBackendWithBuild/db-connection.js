1059a001ff3527fdb5044a115f9a91df800fe0ed
Mahmoud Ibrahim <mahmoudm.ibrahim54@gmail.com>
2021-07-15 22:22:35 +0200

    minor update

--- FullStackBackendWithBuild/db-connection.js
+++ FullStackBackendWithBuild/db-connection.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://Mahmoud:8524625864@cluster0.o2hak.mongodb.net/ReactNode?retryWrites=true&w=majority";
+const url = process.env.MONGO_URI;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
